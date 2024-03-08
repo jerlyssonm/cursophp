cfb2dc8aab73b0084c64e047af55328615564c9c
jerlyssonm <95264940+jerlyssonm@users.noreply.github.com>
2024-03-08 20:03:24 -0300

    tratando erro de envio vazio

--- de001/result.php
+++ de001/result.php
@@ -11,8 +11,14 @@
         <h1>Resultado</h1>
         <?php 
             $result = $_GET["entrada"];
-            $ant = $result - 1;
-            $suc = $result + 1;
+            if($result){
+                $ant = $result - 1;
+                $suc = $result + 1;
+            } else {
+                $result = 0;
+                $ant = $result - 1;
+                $suc = $result + 1;
+            }                
             echo "O numero escolhido foi: <strong>$result </strong>";
          ?> <br>
         <?php echo "O seu antecessor é: <strong>$ant </strong> "?> <br>
