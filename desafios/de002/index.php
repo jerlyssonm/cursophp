8c2136436176befcc7665d612b1843ec02ca0dbf
jerlyssonm <95264940+jerlyssonm@users.noreply.github.com>
2024-03-08 21:44:15 -0300

    criando desafio 2

---
+++ desafios/de002/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 2</title>
+</head>
+<body>
+    <h1>Gerando numeros Aleatorios</h1>
+    <p>Gerando numero aleatorio entre 0 e 100</p>
+    <?php 
+
+        $min = 0;
+        $max = 100;
+        $num = random_int($min, $max);
+       
+        echo "O valor gerado foi $num";
+    ?>
+    <button type="button" onclick="javascript:document.location.reload()"> Gerar Numero</button>
+</body>
+</html>
