b207a9347d975f4d6608a2788a57dc5aabafe493
jerlyssonm <95264940+jerlyssonm@users.noreply.github.com>
2024-03-10 19:27:07 -0300

    desafio 2 finalizado

--- desafios/de002/index.php
+++ desafios/de002/index.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 2</title>
+    <link rel="stylesheet" href="styl.css">
 </head>
 <body>
-    <h1>Gerando numeros Aleatorios</h1>
-    <p>Gerando numero aleatorio entre 0 e 100</p>
-    <?php 
-
-        $min = 0;
-        $max = 100;
-        $num = random_int($min, $max);
-       
-        echo "O valor gerado foi $num";
-    ?>
-    <button type="button" onclick="javascript:document.location.reload()"> Gerar Numero</button>
+    <main>
+        <h1>Gerando numeros Aleatorios</h1>
+        <p>Gerando numero aleatorio entre 0 e 100</p>
+        <?php
+            $min = 0;
+            $max = 100;
+            $num = random_int($min, $max);
+        
+            echo "O valor gerado foi <strong>$num</strong>";
+        ?>
+        <button type="button" onclick="javascript:document.location.reload()"> Gerar Numero</button>
+    </main>
 </body>
 </html>
