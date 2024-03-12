83e6777a8bffc2bd82319a1cd5e3a0c5439252ad
jerlyssonm <95264940+jerlyssonm@users.noreply.github.com>
2024-03-12 15:06:24 -0300

    finalizando desafio 6
    analise de divisão

---
+++ desafios/de006/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 06</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main>
+        <h1>Divisão</h1>
+        <form action="<?=$_SERVER["PHP_SELF"]?>" method="get">
+            <label for="v1">Dividendo</label>
+            <input type="number" name="v1" id="v1">
+            <label for="v2">Divisor</label>
+            <input type="number" name="v2" id="v2">
+            <input type="submit" value="Calcular">
+        </form>
+    </main>
+    <section>
+        <?php 
+            $dividendo = $_GET["v1"] ?? 0;
+            $divisor = $_GET["v2"] ?? 0;
+
+            $resultado = intdiv($dividendo, $divisor);
+            $resto = $dividendo % $divisor;
+
+        ?>
+        <h2>Resultado</h2>
+        <table class="divisao">
+            <tr>
+                <td><?=$dividendo?></td>
+                <td><?=$divisor?></td>
+            </tr>
+            <tr>
+                <td><?=$resto?></td>
+                <td><?=$resultado?></td>
+            </tr>
+        </table>
+    </section>
+</body>
+</html>
